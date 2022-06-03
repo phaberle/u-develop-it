7ea6a08e83c90a1875660080ab777aae2d2e4fd2
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-06-02 23:47:41 -0500

    added parties routes and PUT to change cand. party id

--- server.js
+++ server.js
@@ -34,14 +34,15 @@ app.get('/', (req, res) => {
     });
 });
 
-
+//      <==CANDIDATES==>
+// All candidates with party name
 app.get('/api/candidates', (req, res) => {
     const sql = `SELECT candidates.*, parties.name  AS party_name
                 FROM candidates  
                 LEFT JOIN parties  
                 ON candidates.party_id = parties.id;`
-    
-       db.query(sql, (err, rows) => {
+
+    db.query(sql, (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
@@ -118,6 +119,92 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
+//      <==PARTIES==>
+//All Parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data: rows
+        });
+    });
+});
+
+//Select single party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties where id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+//delete one row from party tbl
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+            //checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: "Party not found!"
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+
+//Update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+    /*Candidate is allowed to not have party affiliation
+    This now forces any PUT request to /api/candidate/:id to include a party_id property. 
+    Even if the intention is to remove a party affiliation by setting it to null, the party_id property is still required.
+    */
+    const errors = inputCheck(req.body,'party_id');
+    if(errors){
+        res.status(400).json({error:errors});
+        return;
+    }
+    const sql = `UPDATE candidates SET party_id =?
+    WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            //check if a record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found!'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
 
 
 //Default response for any other requests not found <-- placement is critical. Is a catchall so make sure it in on the bottom
