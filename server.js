597cae592f23418b6a8b1f7d34fa352c9ac3f99a
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-06-02 21:06:08 -0500

    because it's Thursday

--- server.js
+++ server.js
@@ -1,22 +1,122 @@
+const mysql = require("mysql2");
 const express = require('express');
-const PORT = process.env.PORT||3001;
+const { rootCertificates } = require("tls");
+const res = require("express/lib/response");
+const inputCheck = require('./utils/inputCheck');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 //Express middleware
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//Start Express on port 3001
-app.listen(PORT, () =>{
-    console.log(`Server running on port ${PORT}`); 
-});
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySql username,
+        user: 'dbuser',
+        //Your MySQL password
+        password: 'dbuser',
+        database: 'election'
+    },
+    console.log('Connected to election database.')
+)
+
 
 /*
 We chose the route method get() and the response method res.json() to send the 
 response message "Hello World!" back to the client.
 */
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.json({
-        message:"Hello World!"
+        message: "Hello World!"
     });
+});
+
+
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+//Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+//Delete Candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found!'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+
+//Create Candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES(?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+
+
+//Default response for any other requests not found <-- placement is critical. Is a catchall so make sure it in on the bottom
+app.use((req, res) => {
+    res.status(404).end();
+})
+
+//Start Express on port 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
